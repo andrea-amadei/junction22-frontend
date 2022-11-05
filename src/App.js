8b0dee73f92f16709a84eae270498d92f68be405
Nimer Singh <nasbaweja@gmail.com>
2022-11-05 02:27:06 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Home from './Components/home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <div>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route> */}
+          <Route exact path='/' element={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
